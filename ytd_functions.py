1d831430e1ef4ce244e1c68e7d33b4fb6dbb0ba2
Joao <39555841+joaov777@users.noreply.github.com>
2022-02-23 12:16:13 -0300

    Adding ytd_functions
    Module ytd_functions created

---
+++ ytd_functions.py
@@ -0,0 +1,55 @@
+
+# REQUIRED LIBRARIES
+from pytube import YouTube
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from os import system, name
+import time
+
+standard_header=">> YOUTUBE VIDEO DOWNLOADER <<"
+
+#for headers and better UI
+def reloader(header, screen_clear="False"):
+
+    if screen_clear == True:
+        clear_screen()
+
+    print(f"{header}")
+
+# clearing the screen 
+def clear_screen():
+    system("cls") if name == "nt" else system("clear")
+
+def check_timestamps(start_time, end_time):
+    """Check whether timestamps make sense for the pattern HH:MM:SS"""
+    pass
+
+def check_url(url):
+    """Check Youtube url parameter"""
+    pass
+
+# downloading the video from youtube
+def download_video(video_url, final_video_name):
+
+    print("- Downloading your video...")
+    
+    yt = YouTube(video_url)
+
+    video = yt.streams.get_highest_resolution()
+    video.download(filename=f"{final_video_name}.mp4")
+
+# cutting the video by section
+def cut_video(video_local_path, start_time, end_time, final_file):
+
+    print("- Cutting your video...")
+    ffmpeg_extract_subclip(r"{}".format(video_local_path), time_to_sec(start_time), time_to_sec(end_time), targetname=f"{final_file}.mp4")
+
+# downloading and cutting 
+def download_and_cut_video(video_url, video_name_after_cut, start_time, end_time):
+    
+    download_video(video_url,"temp_video") 
+    cut_video(r"./temp_video.mp4", start_time, end_time, video_name_after_cut)
+ 
+#converting time to seconds
+def time_to_sec(t):
+   h, m, s = map(int, t.split(':'))
+   return h * 3600 + m * 60 + s
