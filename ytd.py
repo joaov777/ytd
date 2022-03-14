7869e8848ede37ff2ec5bd1eb7c79bda2b652714
joaov777 <joaov777@gmail.com>
2022-03-14 15:52:37 -0300

    inserting command line options for script

--- ytd.py
+++ ytd.py
@@ -7,6 +7,49 @@ from datetime import datetime
 import time as t
 from pytube import YouTube
 from pytube.cli import on_progress
+import argparse
+import sys
+
+
+def check_arguments():
+
+    
+    
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest='')
+
+    group = parser.add_mutually_exclusive_group()
+
+    parser.add_argument('--url','-u', type=str, help='Valid Youtube video URL', required=True)
+
+    group.add_argument('--audio','-a', help='Audio stream', action='store_true', default='void')
+    group.add_argument('--video','-v', help='Video stream', action='store_true', default='void')
+    parser.add_argument('--start','-s', type=str, help='Start point for trimming', default='void')
+    parser.add_argument('--end','-e', type=str, help='End point for trimming', default='void')
+    parser.add_argument('--output','-o', type=str, help='Output file name', default='standard')
+
+    args = parser.parse_args()
+
+    yt = YouTube(args.url,on_progress_callback=on_progress)
+
+    if args.video == "void":
+        if args.start == "void":
+            ytdf.download_audio(yt,args.output)
+        else:
+            ytdf.cut_audio(yt,args.start, args.end, args.output)
+    else:
+        if args.start == "void":
+            ytdf.download_video(yt,args.output)
+        else:
+            ytdf.cut_video(yt, args.start, args.end, args.output)
+
+    
+
+def check_args():
+    if not len(sys.argv) > 1:
+        main()
+    else: 
+        check_arguments()
 
 #main thread
 def main():
@@ -114,4 +157,4 @@ def main():
             break
 
 if __name__ == "__main__":
-   main()
+   check_args()
