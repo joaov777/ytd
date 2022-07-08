b46918735f6bcea50df08b311d6ab55019272a19
joao.galvino <joao.galvino@ifce.edu.br>
2022-07-08 09:02:57 -0300

    a few additional lines removed on container.sh

--- container.sh
+++ container.sh
@@ -5,9 +5,4 @@ SCRIPT_DIR=$(dirname "$SCRIPT_FILE") # script directory
 PROJECT_DIR=$(dirname $(dirname $(dirname $SCRIPT_DIR))) #base file system (/)
 CONTAINER_NAME="ytd"
 
-echo $SCRIPT_DIR
-echo $SCRIPT_FILE
-echo $PROJECT_DIR
-
-
 docker run -it --rm --name $CONTAINER_NAME -v $SCRIPT_DIR:/ytd -w /ytd python:3.10.0 bash -c "pip install -r /ytd/requirements.txt && python /ytd/ytd.py"
